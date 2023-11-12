c918fb13b33d9815f4652083e6c8826ea8357125
Lawson Lam <109973600+Pararcana@users.noreply.github.com>
2023-11-12 20:32:50 +0000

    Update caesars_cipher.js

--- JavaScript Algorithms and Data Structures/caesars_cipher.js
+++ JavaScript Algorithms and Data Structures/caesars_cipher.js
@@ -2,8 +2,7 @@ function rot13(str) {
   let ciphertext = str.toUpperCase();
   let plaintext = [];
 
-  for (let i = 0; i < ciphertext.length; i++) {
-    let char = ciphertext[i];
+  for (let char of ciphertext) {
     if (65 <= char.charCodeAt(0) && char.charCodeAt(0) <= 90) {
       let newChar = char.charCodeAt(0) - 13;
       plaintext.push(String.fromCharCode(newChar < 65 ? newChar + 26 : newChar));
