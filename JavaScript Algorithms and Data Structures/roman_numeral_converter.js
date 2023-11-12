e3ef2d81150c1e8041d3699c670519b2eeea724c
Lawson Lam <109973600+Pararcana@users.noreply.github.com>
2023-11-12 20:31:09 +0000

    Update roman_numeral_converter.js

--- JavaScript Algorithms and Data Structures/roman_numeral_converter.js
+++ JavaScript Algorithms and Data Structures/roman_numeral_converter.js
@@ -10,7 +10,7 @@ function convertToRoman(num) {
   num = String(num).padStart(4, "0");
   let ans = [];
   for (let i = 0; i < num.length; i++) {
-    const n = parseInt(num[i]);
+    let n = Number(num[i]);
     for (let j = 0; j < cast[n].length; j++) {
       ans.push(roman[i][cast[n][j]]);
     }
