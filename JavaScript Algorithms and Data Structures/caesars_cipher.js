a21f9ddf1afd927b3bed7579d406ff1e955d008a
Lawson Lam <109973600+Pararcana@users.noreply.github.com>
2023-11-12 17:50:39 +0000

    Create caesars_cipher.js

---
+++ JavaScript Algorithms and Data Structures/caesars_cipher.js
@@ -0,0 +1,15 @@
+function rot13(str) {
+  let ciphertext = str.toUpperCase();
+  let plaintext = [];
+
+  for (let i = 0; i < ciphertext.length; i++) {
+    let char = ciphertext[i];
+    if (65 <= char.charCodeAt(0) && char.charCodeAt(0) <= 90) {
+      let newChar = char.charCodeAt(0) - 13;
+      plaintext.push(String.fromCharCode(newChar < 65 ? newChar + 26 : newChar));
+    } else {
+      plaintext.push(char);
+    }
+  }
+  return plaintext.join("")
+}
