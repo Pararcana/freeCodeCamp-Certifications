ac556000fc680dfb2a7a939c6645f309ab06003f
Lawson Lam <109973600+Pararcana@users.noreply.github.com>
2023-11-25 20:13:50 +0000

    Update telephone_number_validator.js

--- JavaScript Algorithms and Data Structures/telephone_number_validator.js
+++ JavaScript Algorithms and Data Structures/telephone_number_validator.js
@@ -1,8 +1,7 @@
 function telephoneCheck(str) {
-  let re_check = /[(]?[- ]?\d{3}[)]?[- ]?\d{3}[- ]?\d{4}/.test(str);
+  let re_check = /^1?\s?[(]?[- ]?\d{3}[)]?[- ]?\d{3}[- ]?\d{4}(?![)])/.test(str);
   let brackets_check = str.split("(").length === str.split(")").length;
-  let start_check = !(str[0] === "-" || str[str.length-1] === ")")
   str = Array.from(str).filter(v => Array.from("0123456789").includes(v));
-  let length_check = str.length === 10 || (str.length === 11 && str[0] === "1")
-  return Array(start_check, re_check, brackets_check, length_check).every(Boolean);
+  let length_check = str.length === 10 || (str.length === 11 && str[0] === "1");
+  return Array(re_check, brackets_check, length_check).every(Boolean);
 }
